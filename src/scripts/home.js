3925da967d8c141464a8745e3c4ae844063b9f43
inescodefam <ines.codefam@gmail.com>
2024-10-15 16:19:00 +0200

    Add text cursor

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,32 +1,45 @@
 const LETTER_APPEARANCE_DELAY = 100;
+const homeSubtitleText = "Zaiskri.";
+const homeTitleText = "Budi izvrstan u onom što voliš.";
+
+function createCursor() {
+  const cursorElement = document.createElement("span");
+  cursorElement.classList.add("text-cursor");
+
+  return cursorElement;
+}
 
 function animateTitle() {
   const homeTitleElement = document.getElementById("home-title");
-  const homeTitleText = "Budi izvrstan u onom što voliš.";
-  const homeSubtitleText = "\nZaiskri.";
-  var animationDuration = 0;
+  const cursorElement = createCursor();
+  homeTitleElement.appendChild(cursorElement);
 
   homeTitleText.split("").forEach((letter, index) => {
-    animationDuration += LETTER_APPEARANCE_DELAY;
     setTimeout(() => {
       const span = document.createElement("span");
       span.innerText = letter;
-      homeTitleElement.appendChild(span);
+      homeTitleElement.insertBefore(span, cursorElement);
     }, index * LETTER_APPEARANCE_DELAY);
   });
+}
+
+function animateSubtitle() {
+  const animationDelay = LETTER_APPEARANCE_DELAY * homeTitleText.length;
+  const homeSubtitleElement = document.getElementById("home-subtitle");
+  const cursorElement = createCursor();
 
   setTimeout(() => {
-    console.log(animationDuration);
+    homeSubtitleElement.appendChild(cursorElement);
     homeSubtitleText.split("").forEach((letter, index) => {
       setTimeout(() => {
         const span = document.createElement("span");
         span.innerText = letter;
-        homeTitleElement.appendChild(span);
+        homeSubtitleElement.insertBefore(span, cursorElement);
       }, index * LETTER_APPEARANCE_DELAY);
     });
-  }, animationDuration);
+  }, animationDelay);
 }
-
 document.addEventListener("DOMContentLoaded", () => {
   animateTitle();
+  animateSubtitle();
 });
