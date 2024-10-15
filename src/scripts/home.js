0db2da78a5ea7633a107410fa18e02d1ce491999
inescodefam <ines.codefam@gmail.com>
2024-10-15 15:38:49 +0200

    Add subtitle animation

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -3,14 +3,28 @@ const LETTER_APPEARANCE_DELAY = 100;
 function animateTitle() {
   const homeTitleElement = document.getElementById("home-title");
   const homeTitleText = "Budi izvrstan u onom što voliš.";
+  const homeSubtitleText = "\nZaiskri.";
+  var animationDuration = 0;
 
   homeTitleText.split("").forEach((letter, index) => {
+    animationDuration += LETTER_APPEARANCE_DELAY;
     setTimeout(() => {
       const span = document.createElement("span");
       span.innerText = letter;
       homeTitleElement.appendChild(span);
     }, index * LETTER_APPEARANCE_DELAY);
   });
+
+  setTimeout(() => {
+    console.log(animationDuration);
+    homeSubtitleText.split("").forEach((letter, index) => {
+      setTimeout(() => {
+        const span = document.createElement("span");
+        span.innerText = letter;
+        homeTitleElement.appendChild(span);
+      }, index * LETTER_APPEARANCE_DELAY);
+    });
+  }, animationDuration);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
