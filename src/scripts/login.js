bca4bafe9c95a02be412bfa8f5e27a3e018cd311
inescodefam <ines.codefam@gmail.com>
2024-11-03 12:13:59 +0100

    Login

---
+++ src/scripts/login.js
@@ -0,0 +1,39 @@
+function handleError(error) {
+  document.getElementById("error-message").style.display = "block";
+  document.getElementById("error-message").innerText = error;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
+
+  fetch("https://www.fulek.com/data/api/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(async (response) => {
+      const { isSuccess, errorMessages } = await response.json();
+
+      if (isSuccess) {
+        // TODO
+      } else {
+        throw errorMessages.join(", ");
+      }
+    })
+    .catch(handleError);
+}
+
+function addEventListenerToLoginForm() {
+  document.getElementById("loginForm").addEventListener("submit", handleSubmit);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  addEventListenerToLoginForm();
+});
