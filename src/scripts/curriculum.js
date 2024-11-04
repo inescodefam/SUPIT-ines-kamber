feb0781a07a9286bb655d89655d192008fee5bf0
inescodefam <ines.codefam@gmail.com>
2024-11-04 18:32:12 +0100

    Insert to table

--- src/scripts/curriculum.js
+++ src/scripts/curriculum.js
@@ -27,11 +27,45 @@ function displaySubjects(subjects) {
   subjects.forEach((subject) => {
     const option = document.createElement("option");
     option.value = subject.kolegij;
+    option.id = subject.id;
     subjectsDatalist.appendChild(option);
   });
+}
+
+function displaySubjectDetailsInTable(subject) {
+  const subjectDetailsTable = document.getElementById("subject-details-table");
+  const subjectDetailsData = [
+    subject.kolegij,
+    subject.ects,
+    subject.sati,
+    subject.predavanja,
+    subject.vjezbe,
+    subject.tip,
+  ];
+
+  const tableRow = document.createElement("tr");
+
+  subjectDetailsData.forEach((columnData) => {
+    const tableData = document.createElement("td");
+    tableData.innerText = columnData;
+    tableRow.appendChild(tableData);
+  });
+  subjectDetailsTable.appendChild(tableRow);
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
   const subjects = await getAllSubjects();
   displaySubjects(subjects);
+
+  const subjectForm = document.getElementById("subject-form");
+  subjectForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const subjectInputValue = document.getElementById("subject-input")?.value;
+
+    const selectedSubject = subjects.find(
+      (subject) => subject.kolegij === subjectInputValue
+    );
+    displaySubjectDetailsInTable(selectedSubject);
+  });
 });
