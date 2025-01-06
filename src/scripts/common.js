c37ad12f1cddeecc038e1c4683b56730f8d4ad5f
inescodefam <ines.codefam@gmail.com>
2025-01-06 19:01:12 +0100

    Refactor navigation styles for improved visibility; implement dynamic hiding on scroll and restructure curriculum page layout

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const navbars = document.querySelectorAll("nav");
   let lastScrollTop = 0;
   window.addEventListener("scroll", function () {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-      document.body.classList.add("scrolling-down");
-    } else {
-      document.body.classList.remove("scrolling-down");
-    }
+    navbars.forEach((navbar) => {
+      if (scrollTop > lastScrollTop) {
+        navbar.classList.add("nav-hide");
+      } else {
+        navbar.classList.remove("nav-hide");
+      }
+    });
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    console.log(lastScrollTop);
   });
 });
