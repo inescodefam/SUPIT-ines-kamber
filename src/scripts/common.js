5cfe1723fb49ede0bbd08b462bab6b1c5769744d
inescodefam <ines.codefam@gmail.com>
2025-01-06 18:23:15 +0100

    Refactor navigation structure and styles for consistency; enhance scroll detection functionality

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,10 +1,12 @@
-let lastScrollTop = 0;
-window.addEventListener("scroll", function () {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    document.body.classList.add("scrolling-down");
-  } else {
-    document.body.classList.remove("scrolling-down");
-  }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+document.addEventListener("DOMContentLoaded", () => {
+  let lastScrollTop = 0;
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      document.body.classList.add("scrolling-down");
+    } else {
+      document.body.classList.remove("scrolling-down");
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  });
 });
