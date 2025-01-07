f6d7cd86e57e1a63aa1ec5c5328fcf07a66738a3
inescodefam <ines.codefam@gmail.com>
2025-01-07 20:05:05 +0100

    Remove unused mobile-only styles and adjust navbar behavior on scroll for improved layout

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navbars = document.querySelectorAll("nav");
-  const main = document.querySelector("main");
   let lastScrollTop = 0;
   window.addEventListener("scroll", function () {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
     navbars.forEach((navbar) => {
       if (scrollTop > lastScrollTop) {
         navbar.classList.add("nav-hide");
-        main.style.marginTop = "0px";
       } else {
         navbar.classList.remove("nav-hide");
-        main.style.marginTop = "50px";
       }
     });
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
