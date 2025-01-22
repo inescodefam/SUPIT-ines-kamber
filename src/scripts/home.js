99616db1f7e1b9bdf6e53f48d7d0e42715ae5980
inescodefam <ines.codefam@gmail.com>
2025-01-22 12:42:53 +0100

    Enhance typing animation for home title and subtitle with improved cursor visibility and styling

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -3,16 +3,22 @@ const DELETED = "idiš.";
 const TYPED = "oliš.";
 const SUBTITLE = "ZAISKRI.";
 
-const title = document.getElementById("home-title");
-const subtitle = document.getElementById("home-subtitle");
+const title = document.getElementById("home-title").querySelector(".text");
+const subtitle = document
+  .getElementById("home-subtitle")
+  .querySelector(".text");
+const cursor1 = document.getElementById("home-title").querySelector(".cursor");
+const cursor2 = document
+  .getElementById("home-subtitle")
+  .querySelector(".cursor");
+
 var speed = Math.random() * 250;
 
 function typeNextChar(element, text, index, callback) {
   if (index < text.length) {
-    console.log(text.charAt(index));
     element.textContent += text.charAt(index);
     index++;
-    setTimeout(() => typeNextChar(element, text, index, callback), speed);
+    setTimeout(() => typeNextChar(element, text, index, callback), 250);
   } else if (callback) {
     callback();
   }
@@ -25,7 +31,7 @@ function deleteChars(element, content, callback) {
     if (index > 0) {
       element.textContent = element.textContent.slice(0, -1);
       index--;
-      setTimeout(deleteNextChar, speed);
+      setTimeout(deleteNextChar, 250);
     } else if (callback) {
       callback();
     }
@@ -35,10 +41,13 @@ function deleteChars(element, content, callback) {
 }
 
 function homeAnimation() {
+  cursor2.classList.add("hidden");
   typeNextChar(title, TITLE, 0, () =>
     typeNextChar(title, DELETED, 0, () =>
       deleteChars(title, DELETED, () =>
         typeNextChar(title, TYPED, 0, () => {
+          cursor1.classList.add("hidden");
+          cursor2.classList.remove("hidden");
           typeNextChar(subtitle, SUBTITLE, 0, () => {});
         })
       )
