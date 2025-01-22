1306fc57b23c88cb68744124bac74e04e9f9a9f2
inescodefam <ines.codefam@gmail.com>
2025-01-22 11:35:29 +0100

    Add typing animation for home title and subtitle with cursor effect

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,24 +1,49 @@
-function homeAnimation() {
-  new TypeIt("#home-title", {
-    speed: 75,
-    startDelay: 900,
-    lifeLike: true,
-  })
-    .type("Budi izvrstan u onom što vidiš.", {
-      delay: 200,
-    })
-    .delete(5)
-    .type("oliš.", { delay: 100 })
-    .break({ delay: 600 })
-    .type(
-      "<span style='text-transform:uppercase' class='red-text'>Zaiskri. </span>",
-      {
-        delay: 100,
-      }
-    )
-    .go();
+const TITLE = "Budi izvrstan u onom što v";
+const DELETED = "idiš.";
+const TYPED = "oliš.";
+const SUBTITLE = "ZAISKRI.";
+
+const title = document.getElementById("home-title");
+const subtitle = document.getElementById("home-subtitle");
+var speed = Math.random() * 250;
+
+function typeNextChar(element, text, index, callback) {
+  if (index < text.length) {
+    console.log(text.charAt(index));
+    element.textContent += text.charAt(index);
+    index++;
+    setTimeout(() => typeNextChar(element, text, index, callback), speed);
+  } else if (callback) {
+    callback();
+  }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  homeAnimation();
-});
+function deleteChars(element, content, callback) {
+  let index = content.length;
+
+  function deleteNextChar() {
+    if (index > 0) {
+      element.textContent = element.textContent.slice(0, -1);
+      index--;
+      setTimeout(deleteNextChar, speed);
+    } else if (callback) {
+      callback();
+    }
+  }
+
+  deleteNextChar();
+}
+
+function homeAnimation() {
+  typeNextChar(title, TITLE, 0, () =>
+    typeNextChar(title, DELETED, 0, () =>
+      deleteChars(title, DELETED, () =>
+        typeNextChar(title, TYPED, 0, () => {
+          typeNextChar(subtitle, SUBTITLE, 0, () => {});
+        })
+      )
+    )
+  );
+}
+
+document.addEventListener("DOMContentLoaded", homeAnimation);
